c9280dbeb32bc369dca557d67be593f98795d696
James Graca-Jones <jamesgracajones@gmail.com>
2023-01-15 17:21:39 +0000

    Remove test which only applies if Ship is used invalidly

--- src/modules/tests/ship.test.js
+++ src/modules/tests/ship.test.js
@@ -12,15 +12,8 @@ describe('Ship.isSunk method', () => {
     expect(ship.isSunk()).toBe(false);
   });
 
-  test('Ship is sunk when number of hits first reaches its length', () => {
+  test('Ship is sunk when number of valid hits first reaches its length', () => {
     for (let i = 0; i < shipLength; i += 1) {
-      ship.hit();
-    }
-    expect(ship.isSunk()).toBe(true);
-  });
-
-  test('Ship remains sunk after superfluous hits, although these should not be allowed to reach the ship because no place should be hit more than once', () => {
-    for (let i = 0; i < shipLength + 1; i += 1) {
       ship.hit();
     }
     expect(ship.isSunk()).toBe(true);
