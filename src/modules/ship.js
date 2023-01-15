f3044a67d7f1cf6cf9f22f7e9dfb2214fdedd26d
James Graca-Jones <jamesgracajones@gmail.com>
2023-01-15 17:17:35 +0000

    Write setter and getter for length property of Ship object

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,10 +1,22 @@
+/* eslint-disable no-underscore-dangle */
 // TODO: make this.length and this.hitsTaken private
 export default class Ship {
   constructor(length) {
     this.length = length;
     this.hitsTaken = 0;
   }
-  // this.length should have a setter asserting correct argument
+
+  set length(value) {
+    if (!Number.isInteger(value) || value < 1) {
+      throw Error('The selected length must be an integer greater than 0');
+    }
+    this._length = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
   //   hit() {}
 
   //   isSunk() {}
