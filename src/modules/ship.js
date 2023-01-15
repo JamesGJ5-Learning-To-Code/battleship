eae9b9ac9c72cd29755a068b4cc4427191113013
James Graca-Jones <jamesgracajones@gmail.com>
2023-01-15 17:10:18 +0000

    Make constructor for Ship objects

---
+++ src/modules/ship.js
@@ -0,0 +1,11 @@
+// TODO: make this.length and this.hitsTaken private
+export default class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hitsTaken = 0;
+  }
+  // this.length should have a setter asserting correct argument
+  //   hit() {}
+
+  //   isSunk() {}
+}
