826230bb2c00bc0b3a4014a76ab2ecd353526fe6
James Graca-Jones <jamesgracajones@gmail.com>
2023-01-16 15:23:54 +0000

    Initialise Player's opponent as null

--- src/modules/player.js
+++ src/modules/player.js
@@ -3,7 +3,8 @@ import Gameboard from './gameboard';
 export default class Player {
   constructor(allShipCoordinates) {
     this.ownGameboard = new Gameboard(allShipCoordinates);
+    this.opponent = null;
   }
 
-//   attackOpponent(i, j) {}
+  //   attackOpponent(i, j) {}
 }
