947f5db71b170fddf411eb8e327b8ff8be2e3f40
James Graca-Jones <jamesgracajones@gmail.com>
2023-01-17 16:02:06 +0000

    Make Player.opponent private

--- src/modules/player.js
+++ src/modules/player.js
@@ -3,9 +3,11 @@ import Gameboard from './gameboard';
 export default class Player {
   #ownGameboard;
 
+  #opponent;
+
   constructor(allShipCoordinates) {
     this.#ownGameboard = new Gameboard(allShipCoordinates);
-    this.opponent = null;
+    this.#opponent = null;
   }
 
   attackOpponent(i, j) {
