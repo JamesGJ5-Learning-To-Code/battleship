cc9dcb6a5bbdea9dc692a2ba55afd6af60fa5017
James Graca-Jones <jamesgracajones@gmail.com>
2023-01-17 16:06:22 +0000

    Privatise Computer.attackSequence property

--- src/modules/computer.js
+++ src/modules/computer.js
@@ -2,6 +2,8 @@ import arrayShuffle from 'array-shuffle';
 import Player from './player';
 
 export default class Computer extends Player {
+  #attackSequence
+
   constructor(allShipCoordinates) {
     super(allShipCoordinates);
     this.generateAttackSequence();
@@ -10,16 +12,16 @@ export default class Computer extends Player {
   generateAttackSequence() {
     const iSequence = arrayShuffle([...Array(10).keys()]);
     const jSequence = arrayShuffle([...Array(10).keys()]);
-    this.attackSequence = [];
+    this.#attackSequence = [];
     for (let iIndex = 0; iIndex < iSequence.length; iIndex += 1) {
       for (let jIndex = 0; jIndex < jSequence.length; jIndex += 1) {
-        this.attackSequence.push([iSequence[iIndex], jSequence[jIndex]]);
+        this.#attackSequence.push([iSequence[iIndex], jSequence[jIndex]]);
       }
     }
   }
 
   attackRandomly() {
-    attackCoordinates = this.attackSequence.pop();
+    attackCoordinates = this.#attackSequence.pop();
     this.attackOpponent(attackCoordinates[0], attackCoordinates[1]);
   }
 }
