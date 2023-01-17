dae9ce5d6b41231aa4542c02e26505ec748098a0
James Graca-Jones <jamesgracajones@gmail.com>
2023-01-17 19:43:48 +0000

    Rewrite test suite

--- src/modules/tests/gameboard.test.js
+++ src/modules/tests/gameboard.test.js
@@ -77,17 +77,17 @@ import Gameboard from '../gameboard';
 
 describe('Gameboard.receiveAttack method', () => {
   let gameboard;
-  const allShipEnds = [
-    [
-      [1, 0],
-      [1, 0],
-    ],
-    [
-      [8, 2],
-      [6, 2],
-    ],
-  ];
   beforeEach(() => {
+    const allShipEnds = [
+      [
+        [1, 0],
+        [1, 0],
+      ],
+      [
+        [8, 2],
+        [6, 2],
+      ],
+    ];
     gameboard = new Gameboard(allShipEnds);
   });
 
@@ -106,5 +106,25 @@ describe('Gameboard.receiveAttack method', () => {
       gameboard.receiveAttack(1, 0);
     }
     expect(gameboard.afloatShipQuantity).toBe(1);
+  });
+
+  test('The longer ship being hit just once not affecting afloatShipQuantity', () => {
+    gameboard.receiveAttack(8, 2);
+    expect(gameboard.afloatShipQuantity).toBe(2);
+  });
+
+  test('The longer ship being hit at all positions decrementing afloatShipQuantity', () => {
+    for (let i = 6; i <= 8; i += 1) {
+      gameboard.receiveAttack(i, 2);
+    }
+    expect(gameboard.afloatShipQuantity).toBe(1);
+  });
+
+  test('All ships being hit in all areas making afloatShipQuantity 0', () => {
+    gameboard.receiveAttack(1, 0);
+    for (let i = 6; i <= 8; i += 1) {
+      gameboard.receiveAttack(i, 2);
+    }
+    expect(gameboard.afloatShipQuantity).toBe(0);
   });
 });
